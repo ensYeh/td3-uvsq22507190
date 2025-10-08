e0b1da2869836a7a080b7bc0e54da3f97d2ad2f5
ACHAB Mahdjouba <mahdjouba.achab@ens.uvsq.fr>
2025-10-08 10:33:50 +0200

    Ajout de la méthode addItem pour ajouter un DnsItem en mémoire et mettre à jour le fichier DNS dnsdb.txt afin d'assurer la persistance

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -8,14 +8,17 @@ public class Dns {
 
     private List<DnsItem> items = new ArrayList<>();
 
+    // Je garde le chemin du fichier afin de pouvoir le modifier plus tard
+    private String filePath;
+
     public Dns() throws IOException {
         // Charger le fichier de propriétés
         Properties props = new Properties();
         props.load(new FileInputStream("config.properties"));
-        String path = props.getProperty("dns.file");
+        filePath = props.getProperty("dns.file");
 
         // Lire toutes les lignes du fichier
-        List<String> lignes = Files.readAllLines(Paths.get(path));
+        List<String> lignes = Files.readAllLines(Paths.get(filePath));
 
         // Créer les DnsItem et les ajouter à la liste
         for (String ligne : lignes) {
@@ -50,6 +53,7 @@ public class Dns {
         return null;
     }
 
+    // Méthode pour avoir une liste de DnsItem à partir d'un nom de domaine
     public List<DnsItem> getItems(String domaine) {
         List<DnsItem> result = new ArrayList<>();
 
@@ -66,6 +70,32 @@ public class Dns {
         }
 
         return result;
+    }
+
+    // Méthode pour ajouter un DnsItem en mémoire et mettre à jour le fichier DNS
+    // pour assurer la persistance
+    public void addItem(NomMachine nom, AdresseIP ip) throws IOException {
+        // Vérifier s'il existe déjà une machine ou IP dèjà utilisée
+        for (DnsItem item : items) {
+            if (item.getNomMachine().equals(nom)) {
+                throw new IllegalArgumentException("Nom de machine déjà existant : " + nom);
+            }
+            if (item.getAdresseIP().equals(ip)) {
+                throw new IllegalArgumentException("Adresse IP déjà utilisée : " + ip);
+            }
+        }
+
+        // Ajouter à la liste en mémoire
+        DnsItem newItem = new DnsItem(nom, ip);
+        items.add(newItem);
+
+        // Met à jour le fichier DNS en réécrivant l’ensemble du fichier avec le format
+        // attendu "nomMachine adresseIP"
+        List<String> lignes = new ArrayList<>();
+        for (DnsItem item : items) {
+            lignes.add(item.getNomMachine().toString() + " " + item.getAdresseIP().toString());
+        }
+        Files.write(Paths.get(filePath), lignes);
     }
 
 }
