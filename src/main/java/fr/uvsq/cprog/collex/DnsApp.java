fbd92f7f9322a46fee3fef1eb7d0a011dc5afa5b
ACHAB Mahdjouba <mahdjouba.achab@ens.uvsq.fr>
2025-10-09 21:40:13 +0200

    Ajout de la méthode run() chargée d’interagir avec l’utilisateur pour récupérer la prochaine commande, l’exécuter et afficher le résultat

--- src/main/java/fr/uvsq/cprog/collex/DnsApp.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsApp.java
@@ -11,4 +11,18 @@ public class DnsApp {
         dns = new Dns(); // Initialise la base DNS à partir du fichier
         tui = new DnsTUI(dns); // Interface utilisateur textuelle
     }
+
+    // Boucle principale de l'application, elle récupère les commandes utilisateur
+    // et les exécute jusqu'à la sortie
+    public void run() {
+        System.out.println("=== Application DNS Interactive ===");
+        while (true) {
+            Commande commande = tui.nextCommand(); // Récupère la prochaine commande utilisateur
+            if (commande != null) {
+                commande.execute(); // Exécute la commande
+            } else {
+                System.out.println("Commande invalide !");
+            }
+        }
+    }
 }
